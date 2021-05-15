b2139df61e7ad676dfe6fc05044a893f4af33029
LiseDmrg <lisedem4@gmail.com>
2021-05-15 20:18:31 +0200

    edit books data to fit book description

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,2 +1,2 @@
 DROP TABLE IF EXISTS book;
-CREATE TABLE book(isbn VARCHAR(22) PRIMARY KEY, name VARCHAR(255) NOT NULL, quantity integer);
+CREATE TABLE book(isbn VARCHAR(22) PRIMARY KEY, name VARCHAR(255) NOT NULL, quantity integer, author VARCHAR(255) NOT NULL);
