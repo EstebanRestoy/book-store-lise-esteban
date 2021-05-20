70e4b294b3f5300ddd3eb8cc057b73483bcafcc9
EstebanRestoy <esteban.restoy@etu.uca.fr>
2021-05-20 23:52:58 +0200

    [CHANGE SCRIPT]

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,19 @@
+insert into book
+values ('154871564789', 'Livre 1', 10, 'Will');
+insert into book
+values ('545157454574', 'Livre 2', 2, 'Will');
+insert into book
+values ('141574574788', 'Livre 3', 0, 'Will');
+insert into book
+values ('978069101836', 'Psychology and Alchemy', 0, 'Carl Gustav Jung');
+insert into book
+values ('069101814238', 'Psychological Types', 0, 'Carl Gustav Jung');
+insert into book
+values ('673847824222', 'Drei Abhandlungen zur Sexualtheorie', 0, 'Sigmund Freud');
+insert into book
+values ('141565045788', '白色沙灘旁的鶴龜助產院', 0, 'Ogawa Ito');
+insert into user
+values ("Esteban", "vcshkidufhr723140n8f3n44");
+insert into user
+values ("Lise", "hf9234nfw9034mf02nfswe34");
+
